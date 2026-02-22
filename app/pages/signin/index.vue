329957a6e5453f7cda1a6803a5ff3700fc48e33d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-02-22 18:30:35 +0800

    add layout for login and signup

--- app/pages/signin/index.vue
+++ app/pages/signin/index.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
 import LoginForm from '~/components/auth/LoginForm.vue';
-import AppLogo from '~/components/shared/AppLogo.vue';
+definePageMeta({
+    layout: 'auth',
+});
 </script>
 
 <template>
-    <div class="grid min-h-svh lg:grid-cols-2">
-        <div class="flex flex-col gap-4 p-6 md:p-10">
-            <div class="flex justify-center gap-2 md:justify-start">
-                <AppLogo />
-            </div>
-            <div class="flex flex-1 items-center justify-center">
-                <div class="w-full max-w-xs">
-                    <LoginForm />
-                </div>
-            </div>
-        </div>
-        <div class="bg-muted relative hidden lg:block">
-            <img
-                src="/images/page/signup/sign-up.jpg"
-                alt="Image"
-                class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
-        </div>
-    </div>
+    <LoginForm />
 </template>
--- app/pages/signup/index.vue
+++ app/pages/signup/index.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
 import SignupForm from '~/components/auth/SignupForm.vue';
-import AppLogo from '~/components/shared/AppLogo.vue';
+
+definePageMeta({
+    layout: 'auth',
+});
 </script>
 
 <template>
-    <div class="grid min-h-svh lg:grid-cols-2">
-        <div class="flex flex-col gap-4 p-6 md:p-10">
-            <div class="flex justify-center gap-2 md:justify-start">
-                <AppLogo />
-            </div>
-            <div class="flex flex-1 items-center justify-center">
-                <div class="w-full max-w-xs">
-                    <SignupForm />
-                </div>
-            </div>
-        </div>
-        <div class="bg-muted relative hidden lg:block">
-            <img
-                src="/images/page/signup/sign-up.jpg"
-                alt="Image"
-                class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
-        </div>
-    </div>
+    <SignupForm />
 </template>
