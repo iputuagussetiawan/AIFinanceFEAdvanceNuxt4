f1617919b8be8b148b229f5d991c9832d01b91f9
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-03-04 13:09:13 +0800

    current user view improve

--- app/components/NavUser.vue
+++ app/components/NavUser.vue
@@ -47,10 +47,13 @@ const { isMobile } = useSidebar();
                         size="lg"
                         class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                     >
-                        <Avatar class="h-8 w-8 rounded-lg">
+                        <Avatar
+                            class="h-8 w-8 rounded-full shrink-0 overflow-hidden"
+                        >
                             <AvatarImage
                                 :src="user?.avatar || ''"
                                 :alt="user?.name || 'User Avatar'"
+                                class="aspect-square h-full w-full object-cover"
                             />
                             <AvatarFallback class="rounded-lg">
                                 {{ getInitials(user?.name) }}
@@ -79,10 +82,13 @@ const { isMobile } = useSidebar();
                         <div
                             class="flex items-center gap-2 px-1 py-1.5 text-left text-sm"
                         >
-                            <Avatar class="h-8 w-8 rounded-lg">
+                            <Avatar
+                                class="h-8 w-8 rounded-full shrink-0 overflow-hidden"
+                            >
                                 <AvatarImage
                                     :src="user.avatar || ''"
                                     :alt="user.name || 'User Avatar'"
+                                    class="aspect-square h-full w-full object-cover"
                                 />
                                 <AvatarFallback class="rounded-lg">
                                     {{ getInitials(user.name) }}
